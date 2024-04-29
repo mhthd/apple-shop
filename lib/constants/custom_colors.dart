f06cad79f8257556fd21772a448b37673707fe1e
mhtohidi <mh.tohidi2015@gmail.com>
2024-04-29 17:53:57 +0330

    main screen appbar

---
+++ lib/constants/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color blue = Color(0xff3B5EDF);
+  static const Color grey = Color(0xff858585);
+  static const Color red = Color(0xffD02026);
+  static const Color green = Color(0xfF1DB68B);
+  static const Color white = Color(0xffEEEEEE);
+}
