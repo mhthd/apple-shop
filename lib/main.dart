64acef5537e615644a1befadccace08292c657f8
mhtohidi <mh.tohidi2015@gmail.com>
2024-05-01 11:43:31 +0330

    main screen banner slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:apple_shop/constants/custom_colors.dart';
 import 'package:flutter/material.dart';
 
+import 'widgets/banner_slider.dart';
+
 void main() {
   runApp(const AppleShop());
 }
@@ -15,38 +17,68 @@ class AppleShop extends StatelessWidget {
       home: Scaffold(
         backgroundColor: CustomColors.white,
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 44, right: 44, top: 20),
-            child: Container(
-              height: 46,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 44, right: 44, top: 20),
+                  child: appBar(),
                 ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Row(
-                  children: [
-                    Image.asset('assets/images/icon_apple_blue.png'),
-                    const Spacer(),
-                    const Text(
-                      'جستجوی محصولات',
-                      style: TextStyle(
-                          fontSize: 16,
+              const SliverToBoxAdapter(
+                child: Padding(
+                    padding: EdgeInsets.only(top: 32), child: BannerSlider()),
+              ),
+              const SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 32, right: 44, left: 44),
+                  child: Row(
+                    children: [
+                      Spacer(),
+                      Text(
+                        'دسته بندی',
+                        style: TextStyle(
                           fontFamily: 'SB',
-                          color: CustomColors.grey),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Image.asset('assets/images/icon_search.png'),
-                  ],
+                          fontSize: 12,
+                          color: CustomColors.grey,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Container appBar() {
+    return Container(
+      height: 46,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Row(
+          children: [
+            Image.asset('assets/images/icon_apple_blue.png'),
+            const Spacer(),
+            const Text(
+              'جستجوی محصولات',
+              style: TextStyle(
+                  fontSize: 16, fontFamily: 'SB', color: CustomColors.grey),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Image.asset('assets/images/icon_search.png'),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/banner_slider.dart
@@ -0,0 +1,55 @@
+import 'package:apple_shop/constants/custom_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class BannerSlider extends StatelessWidget {
+  const BannerSlider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    PageController pageController =
+        PageController(viewportFraction: 0.83, initialPage: 1);
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        SizedBox(
+          height: 177,
+          child: PageView.builder(
+              itemCount: 3,
+              controller: pageController,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: Container(
+                    height: 177,
+                    decoration: const BoxDecoration(
+                      color: CustomColors.green,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                    ),
+                  ),
+                );
+              }),
+        ),
+        Positioned(
+          bottom: 8,
+          child: SmoothPageIndicator(
+            controller: pageController,
+            count: 3,
+            effect: const ExpandingDotsEffect(
+                dotHeight: 5,
+                dotWidth: 5,
+                spacing: 2,
+                expansionFactor: 5,
+                dotColor: Colors.white,
+                activeDotColor: CustomColors.blue),
+          ),
+        )
+      ],
+    );
+  }
+}
