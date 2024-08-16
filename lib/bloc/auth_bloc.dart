9478745bab23e3d1030e5ad49aaff2b6e2c273f5
mhtohidi <mh.tohidi2015@gmail.com>
2024-08-16 12:16:33 +0330

    feat: add authentication bloc
    the main bloc class

---
+++ lib/bloc/auth_bloc.dart
@@ -0,0 +1,16 @@
+import 'package:apple_shop/bloc/auth_event.dart';
+import 'package:apple_shop/bloc/auth_state.dart';
+import 'package:apple_shop/data/repository/auth_repository.dart';
+import 'package:apple_shop/di/di.dart';
+import 'package:bloc/bloc.dart';
+
+final class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final IAuthRepository _repository = locator.get();
+  AuthBloc() : super(AuthInitialState()) {
+    on<AuthLoginRequestEvent>((event, emit) async {
+      emit(AuthLoadingState());
+      var response = await _repository.login(event.username, event.password);
+      emit(AuthResponseState(response));
+    });
+  }
+}
