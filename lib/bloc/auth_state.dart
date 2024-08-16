de2702e5f747f45be55193af3471a7e379e94db4
mhtohidi <mh.tohidi2015@gmail.com>
2024-08-16 11:31:45 +0330

    refactor: rename AuthInitiateState class

--- lib/bloc/auth_state.dart
+++ lib/bloc/auth_state.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 
 abstract final class AuthState {}
 
-final class AuthInitiatelState extends AuthState {}
+final class AuthInitialState extends AuthState {}
 
 final class AuthLoadingState extends AuthState {}
 
