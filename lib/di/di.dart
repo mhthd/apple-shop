4601724f3c00883617a4505569a16dcbca2dadcd
mhtohidi <mh.tohidi2015@gmail.com>
2024-08-20 12:14:28 +0330

    chore: update dependency injection
    add shared preferences component

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -2,12 +2,17 @@ import 'package:apple_shop/data/datasource/auth_datasource.dart';
 import 'package:apple_shop/data/repository/auth_repository.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final locator = GetIt.instance;
 Future<void> getItInit() async {
   // Components
   locator.registerSingleton<Dio>(
       Dio(BaseOptions(baseUrl: 'https://startflutter.ir/api/')));
+
+  SharedPreferences.setMockInitialValues({});
+  locator.registerSingleton<SharedPreferences>(
+      await SharedPreferences.getInstance());
 
   // Data Sources
   locator.registerFactory<IAuthDataSource>(() => AuthRemote());
