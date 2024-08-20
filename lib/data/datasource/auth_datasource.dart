3e485969460d09b1308246f42ccfa39ba149f2ac
mhtohidi <mh.tohidi2015@gmail.com>
2024-08-20 12:45:18 +0330

    fix: rewrite the auth_datasouce file
    because of discarding the changes by accident

---
+++ lib/data/datasource/auth_datasource.dart
@@ -0,0 +1,47 @@
+import 'package:apple_shop/di/di.dart';
+import 'package:apple_shop/util/api_exception.dart';
+import 'package:dio/dio.dart';
+
+abstract class IAuthDataSource {
+  Future<void> register(
+      String username, String password, String passwordConfirm);
+
+  Future<String> login(String username, String password);
+}
+
+class AuthRemote implements IAuthDataSource {
+  final Dio _dio = locator.get();
+
+  @override
+  Future<void> register(
+      String username, String password, String passwordConfirm) async {
+    try {
+      await _dio.post('collections/users/records', data: {
+        'username': username,
+        'password': password,
+        'passwordConfirm': passwordConfirm
+      });
+    } on DioException catch (e) {
+      throw ApiException(e.response?.statusCode, e.response?.data['message']);
+    } catch (e) {
+      throw ApiException(0, 'unknown error');
+    }
+  }
+
+  @override
+  Future<String> login(String username, String password) async {
+    try {
+      var response = await _dio.post('collections/users/auth-with-password',
+          data: {'identity': username, 'password': password});
+      if (response.statusCode == 200) {
+        return response.data?['token'];
+      }
+    } on DioException catch (e) {
+      throw ApiException(e.response?.statusCode, e.response?.data['message']);
+    } catch (e) {
+      throw ApiException(0, 'unknown error');
+    }
+
+    return '';
+  }
+}
