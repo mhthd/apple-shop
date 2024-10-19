55ee2daa2ff4a1efaa5a3aade07fab4f47e2cf3a
mhtohidi <mh.tohidi2015@gmail.com>
2024-10-19 20:10:45 +0330

    chore: update dependency injection
    add banner datasource

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -1,4 +1,5 @@
 import 'package:apple_shop/data/datasource/auth_datasource.dart';
+import 'package:apple_shop/data/datasource/banner_datasource.dart';
 import 'package:apple_shop/data/repository/auth_repository.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
@@ -16,6 +17,7 @@ Future<void> getItInit() async {
 
   // Data Sources
   locator.registerFactory<IAuthDataSource>(() => AuthRemote());
+  locator.registerFactory<IBannerDatasource>(() => BannerRemoteDatasource());
 
   // Repositories
   locator.registerFactory<IAuthRepository>(() => AuthRepository());
