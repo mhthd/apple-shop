1f2840bf0ee09acf8521d32c806a5ebd6ee4bb94
mhtohidi <mh.tohidi2015@gmail.com>
2024-10-19 20:57:35 +0330

    feat: add home bloc states

---
+++ lib/bloc/home/home_state.dart
@@ -0,0 +1,13 @@
+import 'package:apple_shop/widgets/banner.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeState {}
+
+class HomeInitState extends HomeState {}
+
+class HomeLoadingState extends HomeState {}
+
+class HomeResponseState extends HomeState {
+  Either<String, List<Banner>> bannersList;
+  HomeResponseState(this.bannersList);
+}
