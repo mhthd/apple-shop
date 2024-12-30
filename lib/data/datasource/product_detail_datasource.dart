ddb5d6dc3b1136d80dabc901e463fc69a9df1324
mhtohidi <mh.tohidi2015@gmail.com>
2024-12-30 16:35:54 +0330

    chore: get and save product variants from api and handle exceptions

--- lib/data/datasource/product_detail_datasource.dart
+++ lib/data/datasource/product_detail_datasource.dart
@@ -1,10 +1,16 @@
 import 'package:apple_shop/di/di.dart';
 import 'package:apple_shop/model/product_image.dart';
+import 'package:apple_shop/model/product_variant.dart';
+import 'package:apple_shop/model/variant.dart';
+import 'package:apple_shop/model/variant_type.dart';
 import 'package:apple_shop/util/api_exception.dart';
 import 'package:dio/dio.dart';
 
 abstract class IProductDetailDatasource {
   Future<List<ProductImage>> getGalleries();
+  Future<List<Variant>> getVariants();
+  Future<List<VariantType>> getVariantTypes();
+  Future<List<ProductVariant>> getProductVariants();
 }
 
 class ProductDetailRemoteDatasource extends IProductDetailDatasource {
@@ -26,5 +32,56 @@ class ProductDetailRemoteDatasource extends IProductDetailDatasource {
     } catch (e) {
       throw ApiException(0, 'unknown error');
     }
+  }
+
+  @override
+  Future<List<VariantType>> getVariantTypes() async {
+    try {
+      var response = await _dio.get(
+        'collections/variants_type/records',
+      );
+      return response.data['items']
+          .map<VariantType>((jsonObject) => VariantType.mapFromJson(jsonObject))
+          .toList();
+    } on DioException catch (e) {
+      throw ApiException(e.response?.statusCode, e.response?.statusMessage);
+    } catch (e) {
+      throw ApiException(0, 'unknown error');
+    }
+  }
+
+  @override
+  Future<List<Variant>> getVariants() async {
+    try {
+      Map<String, String> queryParams = {
+        'filter': 'product_id="at0y1gm0t65j62j"'
+      };
+      var response = await _dio.get('collections/variants/records',
+          queryParameters: queryParams);
+      return response.data['items']
+          .map<Variant>((jsonObject) => Variant.mapFromJson(jsonObject))
+          .toList();
+    } on DioException catch (e) {
+      throw ApiException(e.response?.statusCode, e.response?.statusMessage);
+    } catch (e) {
+      throw ApiException(0, 'unknown error');
+    }
+  }
+
+  @override
+  Future<List<ProductVariant>> getProductVariants() async {
+    List<Variant> variantsList = await getVariants();
+    List<VariantType> variantTypeList = await getVariantTypes();
+
+    List<ProductVariant> productVariantList = [];
+    for (var varType in variantTypeList) {
+      var varList = variantsList.where((variant) {
+        return variant.typeId == varType.id;
+      }).toList();
+
+      productVariantList.add(ProductVariant(varType, varList));
+    }
+
+    return productVariantList;
   }
 }
--- lib/data/repository/product_detail_repository.dart
+++ lib/data/repository/product_detail_repository.dart
@@ -1,11 +1,17 @@
 import 'package:apple_shop/data/datasource/product_detail_datasource.dart';
 import 'package:apple_shop/di/di.dart';
 import 'package:apple_shop/model/product_image.dart';
+import 'package:apple_shop/model/product_variant.dart';
+import 'package:apple_shop/model/variant.dart';
+import 'package:apple_shop/model/variant_type.dart';
 import 'package:apple_shop/util/api_exception.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class IProductDetailRepository {
   Future<Either<String, List<ProductImage>>> getGalleries();
+  Future<Either<String, List<Variant>>> getVariants();
+  Future<Either<String, List<VariantType>>> getVariantTypes();
+  Future<Either<String, List<ProductVariant>>> getProductVariants();
 }
 
 class ProductDetailRepository extends IProductDetailRepository {
@@ -14,6 +20,36 @@ class ProductDetailRepository extends IProductDetailRepository {
   Future<Either<String, List<ProductImage>>> getGalleries() async {
     try {
       var response = await _productDetailDatasource.getGalleries();
+      return right(response);
+    } on ApiException catch (e) {
+      return left(e.message ?? 'error');
+    }
+  }
+
+  @override
+  Future<Either<String, List<VariantType>>> getVariantTypes() async {
+    try {
+      var response = await _productDetailDatasource.getVariantTypes();
+      return right(response);
+    } on ApiException catch (e) {
+      return left(e.message ?? 'error');
+    }
+  }
+
+  @override
+  Future<Either<String, List<Variant>>> getVariants() async {
+    try {
+      var response = await _productDetailDatasource.getVariants();
+      return right(response);
+    } on ApiException catch (e) {
+      return left(e.message ?? 'error');
+    }
+  }
+
+  @override
+  Future<Either<String, List<ProductVariant>>> getProductVariants() async {
+    try {
+      var response = await _productDetailDatasource.getProductVariants();
       return right(response);
     } on ApiException catch (e) {
       return left(e.message ?? 'error');
