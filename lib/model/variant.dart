9cf6fb998f58fc0abfdc7367eace3f48240f0040
mhtohidi <mh.tohidi2015@gmail.com>
2024-12-30 16:30:00 +0330

    feat: add model for product varianrs

---
+++ lib/model/variant.dart
@@ -0,0 +1,25 @@
+class Variant {
+  String? id;
+  String? name;
+  String? typeId;
+  String? value;
+  String? priceChange;
+
+  Variant(
+    this.id,
+    this.name,
+    this.typeId,
+    this.value,
+    this.priceChange,
+  );
+
+  factory Variant.mapFromJson(Map<String, dynamic> jsonObject) {
+    return Variant(
+      jsonObject['id'],
+      jsonObject['name'],
+      jsonObject['type_id'],
+      jsonObject['value'],
+      jsonObject['price_change'],
+    );
+  }
+}
